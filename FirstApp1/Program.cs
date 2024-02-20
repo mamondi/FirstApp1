30c9f3975da837e0784dee7600557839ab5a3b42
DimamonDESKTOP <dimamon02@gmail.com>
2024-02-20 19:54:36 +0200

    done

--- FirstApp1/Program.cs
+++ FirstApp1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.");
+        Console.WriteLine("Bjarne Stroustrup");
+    }
+}
