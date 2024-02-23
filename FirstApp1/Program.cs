35314489a637671f9fd128f3511494e07a9437f5
DimamonDESKTOP <dimamon02@gmail.com>
2024-02-23 12:19:05 +0200

    done

--- FirstApp1/Program.cs
+++ FirstApp1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a percentage");
+        int percent = Convert.ToInt32(Console.ReadLine());
+
+        int result = (num * percent) / 100;
+        Console.WriteLine("The result is: " + result);
+        
+    }   
+}
