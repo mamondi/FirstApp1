078c1ae8021d23bee64048d3caeffb8e148863c1
DimamonDESKTOP <dimamon02@gmail.com>
2024-02-23 12:00:34 +0200

    done

--- FirstApp1/Program.cs
+++ FirstApp1/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+
+        int formNum = 0;
+
+        for(int i = 0; i <= 3; i++)
+        {
+            Console.WriteLine("Введіть цифру: ");
+            int digit = Convert.ToInt32(Console.ReadLine());
+            formNum = formNum * 10 + digit;
+        }
+        Console.WriteLine("Ваше число: ");
+        Console.WriteLine(formNum);
+    }
+}
